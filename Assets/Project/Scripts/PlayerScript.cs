1df4ba3f8c2d828a6bcb4db369b8aa3ee0f75157
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-25 18:37:32 +0000

    added ball

---
+++ Assets/Project/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public GameObject ball;
+	public GameObject playerCamera;
+
+	public float ballDistance = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		ball.GetComponent<Rigidbody> ().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballDistance);
+	}
+}
