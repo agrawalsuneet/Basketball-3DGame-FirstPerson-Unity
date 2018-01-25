fb3316236c09c483d5784c7142bfe00f8df42fa8
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-25 20:04:38 +0000

    score effects

---
+++ Assets/Project/Scripts/ScoreAreaScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAreaScript : MonoBehaviour {
+
+	public GameObject effectObject;
+
+	void Start (){
+		effectObject.SetActive (false);
+	}
+
+	void OnTriggerEnter (Collider otherCollider){
+		if(null != otherCollider.GetComponent<BallScript>()){
+			effectObject.SetActive (true);
+		}
+	}
+}
