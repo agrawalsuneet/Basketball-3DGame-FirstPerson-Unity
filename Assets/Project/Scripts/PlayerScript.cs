f917fe1d1b18637f300b5da4e6125110ab97ea6e
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-25 19:04:25 +0000

    added ball throw

--- Assets/Project/Scripts/PlayerScript.cs
+++ Assets/Project/Scripts/PlayerScript.cs
@@ -8,6 +8,9 @@ public class PlayerScript : MonoBehaviour {
 	public GameObject playerCamera;
 
 	public float ballDistance = 2.0f;
+	public float ballThrowingForce = 550.0f;
+
+	public bool holdingBall = true;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,15 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (holdingBall) {
 		ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballDistance);
+
+			if (Input.GetMouseButtonDown (0)) {
+				holdingBall = false;
+				ball.GetComponent<Rigidbody> ().useGravity = true;
+				ball.GetComponent<Rigidbody> ().AddForce (playerCamera.transform.forward * ballThrowingForce);
+			}
+			
+		}
 	}
 }
